d7f3a313e0926f6336e3f2662ac910d97b0614d3
Andrew Cheesman <andrewcheesman@gmail.com>
2017-05-31 22:48:42 +0000

    functionalized dtmc calculation

--- tsm1.R
+++ tsm1.R
@@ -2,8 +2,8 @@
 # May 6, 2017
 
 library(ggplot2)
-library(sqldf)
 library(scales)
+library(markovchain)
 
 ####################################################################################################
 # Data from https://archive.ics.uci.edu/ml/machine-learning-databases/00350/
@@ -19,7 +19,6 @@ a <- a1[a1$PAY_0 != -2 &
           a1$PAY_4 != -2 & 
           a1$PAY_5 != -2 & 
           a1$PAY_6 != -2,]
-
 rm(a1)
 
 # Recoding the -1's to 0's (as they seem to be equivalent - signifying being up-to-date on payments)
@@ -32,75 +31,171 @@ for (i in 7:12) {
 
 rm(i)
 
-# Mapping some basic trends
+save(a, file = "base.RDA")
 
-dummy <- data.frame()
-for (i in 7:12) {
-  b <- data.frame(var = colnames(a)[i],
-                  val = a[,i])
-  dummy <- rbind(dummy, b)
+# # Mapping some basic trends
+# 
+# dummy <- data.frame()
+# for (i in 7:12) {
+#   b <- data.frame(var = colnames(a)[i],
+#                   val = a[,i])
+#   dummy <- rbind(dummy, b)
+#   }
+# 
+# rm(b, i)
+# 
+# ggplot(dummy, aes(x = factor(val))) +
+#   geom_histogram(stat = "count") + 
+#   facet_wrap(~var) +
+#   # scale_y_log10() + 
+#   theme_bw()
+# 
+# rm(dummy)rm(dummy)
+
+# Basic Frequencies @ t0 to t6
+
+ad <- data.frame(st = c(0, 2:8))
+for (p in 7:12) {
+  
+  agg <- aggregate(a$ID ~ a[,p], a, FUN = length)
+  colnames(agg) <- c("st", paste0("ct_", substr(colnames(a)[p], 5, 5)))
+  ad <- merge(ad, agg, by = "st", all = T)
+  
   }
 
-rm(b, i)
+rm(p, agg)
 
-ggplot(dummy, aes(x = val)) +
-  geom_histogram(bins = 11) + 
-  facet_wrap(~var) +
-  theme_bw()
-
-rm(dummy)
-
-# Checking occurrence of movement from 1 @ t0 to 3+ at tn
-
-c <- sqldf("select pay_0, pay_2, pay_3, pay_4, pay_5, pay_6, count(distinct(id)) from a group by 1, 2, 3, 4, 5, 6")
-write.csv(c, "prog.csv", row.names = F)
-
+ad_pct <- data.frame(id = ad$st,
+                     st0 = percent(ad[,2] / nrow(a)),
+                     st2 = percent(ad[,3] / nrow(a)),
+                     st3 = percent(ad[,4] / nrow(a)),
+                     st4 = percent(ad[,5] / nrow(a)),
+                     st5 = percent(ad[,6] / nrow(a)),
+                     st6 = percent(ad[,7] / nrow(a)))
 
 ####################################################################################################
-# Calculating TSM probabilities
+# Prep for DTMC and modeling
+# long-term, functionalize based on input dataset (to resolve duplicate code for test/train)
 
-# Normalizing for time
-dummy <- data.frame()
+load("base.RDA")
+
+# Bucketing states - comment out line 84-86 to go back to original
+
+# for (i in 7:12) {
+#   a[,i] <- ifelse(a[,i] >= 3, 3, a[,i])
+# }
+
+# Test/train
+
+set.seed(521)
+trn_ind <- sample(seq_len(nrow(a)), size = floor(0.7 * nrow(a)))
+
+trn <- a[trn_ind, ]
+tst <- a[-trn_ind, ]
+
+# Normalizing for time - transferring all periods to simple before/after
+
+trnl <- data.frame()
 for (i in 7:11) {
-  b <- data.frame(id = a$ID,
-                  a = a[,i],
-                  b = a[,i+1])
-  dummy <- rbind(dummy, b)
+  b <- data.frame(id = trn$ID,
+                  a = trn[,i],
+                  b = trn[,i+1])
+  trnl <- rbind(trnl, b)
 }
 
-# State-dependent frequencies
-unqs <- sort(unique(dummy$a))
+tstl <- data.frame()
+for (i in 7:11) {
+  c <- data.frame(id = tst$ID,
+                  a = tst[,i],
+                  b = tst[,i+1])
+  tstl <- rbind(tstl, c)
+}
 
-sdf <- data.frame(st = c(0:8))
-for (j in 1:length(unqs)) {
-  sdf1 <- aggregate(id ~ b, 
-                   dummy[dummy$a == unqs[j], ], 
-                   FUN = length)
-  colnames(sdf1)[2] <- paste0(unqs[j],"_ct")
-  sdf <- merge(sdf, sdf1, by.x = "st", by.y = "b", all = T)
+rm(b, c, i)
+
+save(trn, file = "train.RDA")
+save(tst, file = "test.RDA")
+
+save(trnl, file = "train_long.RDA")
+save(tstl, file = "test_long.RDA")
+
+rm(list = ls())
+
+# Calculate DTMCs - functionalized
+# Detects states as per contents of both df fields
+
+load("train_long.RDA")
+load("test_long.RDA")
+
+# takes as input long-format data.frame (three fields - "id", "a" (pre), and "b" (post)) formatted as above
+
+mc_calc <- function(df) {
+  
+  # State-dependent frequencies
+  unqs <- sort(unique(c(unique(df$a), 
+                        unique(df$b))))
+  
+  sdf <- data.frame(st = c(unqs))
+  for (j in 1:length(unqs)) {
+    sdf1 <- aggregate(id ~ b, 
+                      df[df$a == unqs[j], ], 
+                      FUN = length)
+    colnames(sdf1)[2] <- paste0(unqs[j],"_ct")
+    sdf <- merge(sdf, sdf1, by.x = "st", by.y = "b", all = T)
   }
-
-rm(j, unqs, sdf1)
-
-sdf$total <- rowSums(sdf[,2:length(colnames(sdf))], na.rm = T)
-
-sdf_pct <- data.frame(id = sdf$st,
-                      sdf[,2] / sdf[,10],
-                      sdf[,3] / sdf[,10],
-                      sdf[,4] / sdf[,10],
-                      sdf[,5] / sdf[,10],
-                      sdf[,6] / sdf[,10],
-                      sdf[,7] / sdf[,10],
-                      sdf[,8] / sdf[,10],
-                      sdf[,9] / sdf[,10])
-
-sdf_pct[is.na(sdf_pct)] <- 0
-sdf_pct[,c(2:9)] <- apply(sdf_pct[,c(2:9)], 2, percent)
-colnames(sdf_pct)[2:9] <- c("pct0", "pct2", "pct3", "pct4", "pct5", "pct6", "pct7", "pct8")
-
-for (k in 2:9) {
-sdf_pct[sdf_pct[,k] == "0.0%", k] <- ""
+  
+  rm(j, sdf1)
+  
+  sdf$total <- rowSums(sdf[,2:length(colnames(sdf))], na.rm = T)
+  
+  for (k in 1:length(unqs)) {
+    sdf$a <- sdf[,1 + k] / sdf$total
+    colnames(sdf)[which(colnames(sdf) == "a")] <- unqs[k]
+  }
+  
+  sdf[is.na(sdf)] <- 0
+  
+  ndx <- which(colnames(sdf) == "total")+1
+  sdf_pct <- sdf[,c(ndx:ncol(sdf))]
+  
+  # Working around the too-strict row sum requirements for package
+  # Adjusts just the first group for the diff (in this case, the largest one, where it'll have the lowest comparative impact)
+  
+  sdf_pct2 <- round(sdf_pct, 3)
+  sdf_pct2$dff <- 1 - rowSums(sdf_pct2)
+  sdf_pct2[, 1] <- sdf_pct2[, 1] + sdf_pct2$dff
+  
+  mat <- data.matrix(sdf_pct2[,-ncol(sdf_pct2)])
+  rownames(mat) <- paste0("st_", unqs)
+  colnames(mat) <- paste0("st_", unqs)
+  
+  dtmc <<- new("markovchain",
+               states = paste0("st_", unqs),
+               byrow = T, 
+               transitionMatrix = mat,
+               name = paste0(deparse(substitute(df))))
 }
+
+mc_calc(trnl)
+assign("train_dtmc", dtmc)
+
+mc_calc(tstl)
+assign("test_dtmc", dtmc)
+
+rm(dtmc)
+
+get("train_dtmc")
+get("test_dtmc")
+
+
+
+
+
+
+
+
+
+# scratch crap
 
 # todo
 # 0. separate into train and test
@@ -110,3 +205,60 @@ sdf_pct[sdf_pct[,k] == "0.0%", k] <- ""
 # 3. models for as many states as there exist
 #    response var: EITHER treat anything BUT a movement back to 0 as a 1 (which means remaining behind on payments) 
 #                  OR build multivariate logistic models
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
